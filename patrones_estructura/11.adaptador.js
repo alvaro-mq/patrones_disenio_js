9ca7e812ac4ca00aa78539ba1de7d1d6d0d07509
alvaro <almamani@agetic.gob.bo>
2020-08-19 16:47:56 -0400

    patron adaptador

---
+++ patrones_estructura/11.adaptador.js
@@ -0,0 +1,52 @@
+// Adaptador
+
+class Api {
+  constructor () {
+    this.operations = function (url, opts, verb) {
+      switch (verb) {
+        case 'get':
+          // return fetch
+        case 'post':
+          // return fetch
+        default:
+          return
+      }
+    };
+  }
+}
+
+class Api2 {
+  constructor() {
+    this.get = function (url, opts) {
+      // return axios.get
+    };
+    this.post = function (url, opts) {
+      // return axios.post
+    };
+  }
+}
+
+class ApiAdapter {
+  constructor () {
+    const api2 = new Api2();
+    this.operations = function (url, opts, verb) {
+      switch (verb) {
+        case 'get':
+          return api2.get(url, opts);
+        case 'post':
+          return api2.post(url, opts);
+        default:
+          return
+      }
+    }
+  }
+}
+
+const api = new Api();
+api.operations('www.google.bo', { q: 1 }, 'get');
+
+const api2 = new Api2();
+api2.get('www.google.bo', { q: 1 });
+
+const adapter = new ApiAdapter();
+adapter.operations('www.google.bo', { q: 1 }, 'get');
