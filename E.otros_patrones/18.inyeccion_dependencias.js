4986b207fbf76d8b1180b7624050c1124c7e95f0
alvaro <almamani@agetic.gob.bo>
2020-08-19 21:02:02 -0400

    inyeccion de dependencias

---
+++ E.otros_patrones/18.inyeccion_dependencias.js
@@ -0,0 +1,12 @@
+// Patron injeccion de dependecias
+
+const express = require('express');
+const axios = require('axios');
+const { get } = require('./handlers');
+
+const app = express();
+const port = 3000;
+
+app.get('/', get(axios));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
